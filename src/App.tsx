e1f693488d6c65383857547bcccb036ac42a729c
Rasheed Sharaf Aldeen <134731625+Rasheed1506993@users.noreply.github.com>
2025-08-05 00:19:08 +0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { HashRouter, Routes, Route } from "react-router-dom";
 import Navbar from "./components/Navbar";
+import Header from "./components/Header";
 import Footer from "./components/Footer";
 import Home from "./pages/Home";
 import About from "./pages/About";
@@ -30,7 +31,7 @@ const App = () => (
       <Sonner />
       <HashRouter>
         <div className="min-h-screen flex flex-col">
-          <Navbar />
+          <Header />
           <main className="flex-grow">
             <Routes>
               <Route path="/" element={<Home />} />
